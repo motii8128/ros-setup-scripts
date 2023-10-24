baa09df76f7993751ffe81c1f014ec1f45483e2e
motii <108280115+motii8128@users.noreply.github.com>
2023-10-24 12:41:22 +0900

    Add files via upload

--- scripts/ros-noetic-setup.bash
+++ scripts/ros-noetic-setup.bash
@@ -11,12 +11,13 @@ if [ $ans = 'y' ]; then
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
     echo "set key"
-    sudo apt install curl -y
+    sudo apt install curl -y > /dev/null
     curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
-    sudo apt update -y
+    echo "update"
+    sudo apt update -y > /dev/null
     echo "Install ROS NOETIC"
-    sudo apt install ros-noetic-desktop-full -y
+    sudo apt install ros-noetic-desktop-full -y > /dev/null
     if [ $? -gt 0 ]; then
         echo Failed to install ros-noetic
         exit 1
@@ -27,7 +28,10 @@ if [ $ans = 'y' ]; then
     source ~/.bashrc
 
     echo install other dependencies
-    sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+    sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y > /dev/null
+    if [ $? -gt 0 ]; then
+        echo Failed to install other dependencies
+        exit 1
 
     echo initialize rosdep
     sudo rosdep init

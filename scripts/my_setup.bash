ff65b6641033061a9ddd163ff4fbfd112d998bf6
motii <108280115+motii8128@users.noreply.github.com>
2023-11-09 22:18:17 +0900

    Update my_setup.bash

--- scripts/my_setup.bash
+++ scripts/my_setup.bash
@@ -2,6 +2,4 @@
 
 echo "Get goole chrome .deb and Install"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-cd ~/
-cd Downloads
 sudo apt install ./google-chrome-stable_current_amd64.deb

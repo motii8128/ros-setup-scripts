0cdaef726f6bdca2c7510cd06a3541ea57c9820d
motii <108280115+motii8128@users.noreply.github.com>
2023-11-09 22:46:29 +0900

    Update and rename my_setup.bash to my_humble_setup.bash

---
+++ scripts/my_humble_setup.bash
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+echo "Start Set ENV??(y/n)"
+read flag
+if [ $flag = 'n' ]; then
+  echo "shutting down"
+  exit 1
+fi
+
+echo "Start install ROS2 Humble"
+Set locale
+(
+  locale
+  sudo apt update && sudo apt install locales
+  sudo locale-gen en_US en_US.UTF-8
+  sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+  export LANG=en_US.UTF-8
+  locale
+) &
+PID1 = $!
+wait $PID1
+
+echo "Get GPG key"
+(
+  sudo apt install software-properties-common
+  sudo add-apt-repository universe
+  sudo apt update && sudo apt install curl -y
+  sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+  sudo apt update
+  sudo apt upgrade
+) &
+PID2 = $!
+wait $PID2
+
+echo "ROS2-Humble"
+(
+  sudo apt install ros-humble-desktop
+) &
+PID3 = $!
+wait $PID3
+
+source /opt/ros/humble/setup.bash
+
+echo "Get goole chrome .deb and Install"
+(
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo apt install ./google-chrome-stable_current_amd64.deb
+) &
+PID4 = $!
+wait $PID4
+
+echo "Get Visual-Studio-Code"
+(
+  curl -s https://raw.githubusercontent.com/karaage0703/ubuntu-setup/master/install-vscode.sh | /bin/bash
+) &
+PID5 = $!
+wait $PID5
+
+sudo apt install ibus-mozc
+
+echo Finished to install my environment

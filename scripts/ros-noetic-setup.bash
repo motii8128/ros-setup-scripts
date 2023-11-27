62f71c490cd5e51f7b34fb7236954d58c9cc4783
motii <108280115+motii8128@users.noreply.github.com>
2023-11-27 21:15:33 +0900

    Update ros-noetic-setup.bash

--- scripts/ros-noetic-setup.bash
+++ scripts/ros-noetic-setup.bash
@@ -57,6 +57,15 @@ if [ $ans = 'y' ]; then
         exit 1
     fi
 
+    echo "Get goole chrome .deb and Install"
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install ./google-chrome-stable_current_amd64.deb -y
+    
+    echo "Get Visual-Studio-Code"
+    curl -s https://raw.githubusercontent.com/karaage0703/ubuntu-setup/master/install-vscode.sh | /bin/bash
+    
+    sudo apt install ibus-mozc -y
+
     echo "All process finished!!!"
 
 else

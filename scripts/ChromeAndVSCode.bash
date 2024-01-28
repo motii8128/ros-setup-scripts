ef2da7a576a4a19e9cabe6dd510f3aea8fdc0aac
motii <108280115+motii8128@users.noreply.github.com>
2024-01-28 18:00:40 +0900

    Create ChromeAndVSCode.bash

---
+++ scripts/ChromeAndVSCode.bash
@@ -0,0 +1,8 @@
+echo "Get goole chrome .deb and Install"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+
+echo "Get Visual-Studio-Code"
+curl -s https://raw.githubusercontent.com/karaage0703/ubuntu-setup/master/install-vscode.sh | /bin/bash
+
+sudo apt install ibus-mozc -y
